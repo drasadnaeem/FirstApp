55c5cf1158d4d142ade6cda74523fb6548598c5a
Asad Naeem <drasadnaeem@gmail.com>
2022-07-23 17:29:09 +0800

    Stage1

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Dimensions, StatusBar, StyleSheet, Text, View, Image, TouchableOpacity, TouchableHighlight, Platform, Button, Alert, SafeAreaView } from 'react-native';
+import { useDimensions } from '@react-native-community/hooks';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+      //<WelcomeScreen/>
+      <ViewImageScreen />
+     
+      
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
